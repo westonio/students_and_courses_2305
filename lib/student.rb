6cd36b008692f974214c2e5b4441474e0dc82b7a
Weston Schutt <wtschutt@gmail.com>
2023-05-24 09:11:35 -0600

    Add Student class and methods

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
 class Student
+
+  attr_reader :name, 
+              :age,
+              :scores
  
+  def initialize(info)
+    @name = info[:name]
+    @age = info[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    scores << score
+  end
+
+  def grade
+    num_scores = scores.length
+    sum = scores.sum
+    sum.to_f / num_scores
+  end
 end
