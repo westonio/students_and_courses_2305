8836129d41aff1007199a3dbeecd04644e62e469
Weston Schutt <wtschutt@gmail.com>
2023-05-24 09:25:05 -0600

    Add Course class and methods

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,23 @@
 class Course
+  attr_reader :name,
+              :capacity,
+              :students
 
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+  end
+
+  def full?
+    students.length == capacity
+  end
+
+  def enroll(student)
+    if !full?
+      students << student 
+    else
+      "Sorry - Class is full!"
+    end
+  end
 end
