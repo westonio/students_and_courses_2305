c9da8fc4e49b28ebbaccb01a2a04ed4502b4de55
Weston Schutt <wtschutt@gmail.com>
2023-05-24 09:54:13 -0600

    Add Gradebook class and tests

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,29 @@
 class Gradebook
+  attr_reader :instructor,
+              :courses
   
+  def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+  end
+
+  def add_course(course)
+    courses << course
+  end
+
+  def list_all_students
+    list = {}
+    courses.map do |course|
+      list[course.name.to_sym] = course.students
+    end
+    list
+  end
+
+  def students_below(threshold)
+    courses.map do |course|
+      course.students.select do |student| 
+        student if student.grade < threshold
+      end
+    end.flatten
+  end
 end
